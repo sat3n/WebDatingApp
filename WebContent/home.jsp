40a44f6d314fccfc05307e7b29fe85d45af2f018
Saten <satenderkundu@hotmail.com>
2020-04-05 19:25:57 +0100

    Adding user info to home page

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,5 +10,26 @@
 <body>
 <h1>This is your Homepage</h1>
 <p>This is where user details will be displayed<p>
+<div class="date-body-footer">
+<!--     <div id="date-body-footer-base" class="date-body-footer"> WebAppDev(EE417) | Assignment- Online Dating System <br><hr> -->
+<!--       www.DateMe.com &nbsp;&nbsp; Satender Kundu | 19211628</div> -->
+
+<c:forEach items="${UserList}" var="user">
+						<tr>
+							<td style="font-family: cursive;" align="center">${user.FIRST_NAME}</td>
+<%-- 							<td style="font-family: cursive;" align="center">${user.price}</td> --%>
+<%-- 							<td style="font-family: cursive;" align="center">${user.currency}</td> --%>
+<!-- 							<td style="font-family: cursive;" align="center"><input -->
+<!-- 								type="submit" id="button" value="Remove"></td> -->
+<!-- 							<td> <input type="hidden" name="itemid" value=${menu.itemid}></td> -->
+							<%-- 							<td><img alt="${menu.itemName}" --%>
+							<%-- 								src="images/${menu.itemid }.jpg " width="400" height="400"> --%>
+							<!-- 								<input type="submit" value="add"></td> -->
+							<!-- 							<td><input type="submit"></td> -->
+							<%-- 							<td>${menu.price}</td> --%>
+							<%-- 							<td>${menu.currency}</td> --%>
+						</tr>
+					</c:forEach>
+</div>
 </body>
 </html>
--- src/com/dateme/LoginServlet.java
+++ src/com/dateme/LoginServlet.java
@@ -1,88 +1,130 @@
 package com.dateme;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-	import java.io.*;
-	import javax.servlet.*;
-	import javax.servlet.annotation.WebServlet;
-	import javax.servlet.http.*;
-	import java.sql.*;
-	import oracle.jdbc.driver.*;
-	import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-	
-	@WebServlet("/LoginServlet")
-	public class LoginServlet extends HttpServlet {
-	 private static final long serialVersionUID = 1L;
-     Connection con = null;
-     Statement stmt = null;
-     ResultSet rs = null;
-     String JDBCUrl = "jdbc:oracle:thin:@ee417.c7clh2c6565n.eu-west-1.rds.amazonaws.com:1521:EE417";
-     String username = "ee_user";
-     String password = "ee_pass";
-     
-	    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-	     res.setContentType("text/html");
-	     PrintWriter out = res.getWriter();
-	     
-	     try {
-	    	 System.out.println("Fecthing User info from database");
-	    	 Class.forName("oracle.jdbc.driver.OracleDriver");
-	    	 con = DriverManager.getConnection(JDBCUrl, username, password);
-	     }
-	     catch(Exception e){
-	    	 e.printStackTrace();
-	     }
-	     
-	     User user=null;
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Connection con = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	String JDBCUrl = "jdbc:oracle:thin:@ee417.c7clh2c6565n.eu-west-1.rds.amazonaws.com:1521:EE417";
+	String username = "ee_user";
+	String password = "ee_pass";
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+
 		try {
-			user = validateUser(req.getParameter("usernamefield"), req.getParameter("passwordfield"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Fecthing User info from database");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(JDBCUrl, username, password);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	     if (user!=null) {   // login succeeded
-	     out.println("Welcome " + user.getFirstname() + " " + user.getSurname() + ".  You are now logged in!");
-	     out.println("<br/><br/>Please click here to go to the <a href=\"AccountHome\">AccountHome page</a>");
-	     HttpSession session = req.getSession();
-	     session.setAttribute("theUser", user);  // put the OBJECT on the session
-	     }
-	     else {   // login failed
-	     // no failure message for now but it will 
-	     // send the user back to try again
-	     res.sendRedirect("login.html");   
-	     }
-	     
-	     out.close();
-	 }
-	    
-	    /*
-	     * This method will dummy the login process and return null if unsuccessful or a User object if successful
-	     */
-	    private User validateUser(String username,String password) throws SQLException{
-	    	
-	    	String query = "SELECT * FROM DateMe_User WHERE USERNAME='" + username + "'";
-            Statement statement = con.createStatement();
-            ResultSet result = statement.executeQuery(query);
-            User user=new User();
-            if (result.next() == false) {
-              
-                System.out.println("Result set is empty! User id does not exist");
-            } else {
-                do {
-                    if (result.getString("password").equals(password)) {
-                    	user.setFirstname(result.getString("FIRST_NAME"));
-                        user.setUsername(result.getString("USERNAME"));
-                        user.setSurname(result.getString("LAST_NAME"));
-                        user.setEmail(result.getString("E_MAIL"));
-                        user.setPassword(result.getString("PASSWORD"));
-                        user.setDob(result.getString("DOB"));
-                        user.setSex(result.getString("SEX"));
-                    } else {
-                    	System.out.println("loda lehsun");
-                    }
-                } while (result.next());
-            }
-	    	
-	    	return user; 
-	    }
+
+		try {
+			User user = validateUser(req.getParameter("usernamefield"), req.getParameter("passwordfield"));
+			if (user.getUsername() != null || user != null) { // login succeeded
+				HttpSession session = req.getSession();
+				session.setAttribute("theUser", user); // put the OBJECT on the session
+				List<User> userList = fetchAllUsers();
+				req.setAttribute("UserList", userList);
+				RequestDispatcher requestDispatcher = req.getRequestDispatcher("home.jsp");
+				requestDispatcher.forward(req, res);
+			}
+//			else { // login failed
+//				res.sendRedirect("login.html");
+//			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+//		out.close();
 	}
+
+	/*
+	 * This method will dummy the login process and return null if unsuccessful or a
+	 * User object if successful
+	 */
+	private User validateUser(String username, String password) throws SQLException {
+
+		String query = "SELECT * FROM DateMe_User WHERE USERNAME='" + username + "'";
+		Statement statement = con.createStatement();
+		ResultSet result = statement.executeQuery(query);
+		User user = null;
+		if (result.next() == false) {
+
+			System.out.println("Result set is empty! User id does not exist");
+		} else {
+			do {
+				if (result.getString("PASSWORD").equals(password)) {
+					user = new User();
+					user.setFirstname(result.getString("FIRST_NAME"));
+					user.setUsername(result.getString("USERNAME"));
+					user.setSurname(result.getString("LAST_NAME"));
+					user.setEmail(result.getString("E_MAIL"));
+					user.setPassword(result.getString("PASSWORD"));
+					user.setDob(result.getString("DOB"));
+					user.setSex(result.getString("SEX"));
+				} 
+//				else {
+//					System.out.println("loda lehsun");
+//				}
+			} while (result.next());
+		}
+
+		return user;
+	}
+
+	/*
+	 * This method will dummy the login process and return null if unsuccessful or a
+	 * User object if successful
+	 */
+	private List<User> fetchAllUsers() throws SQLException {
+
+		String query = "SELECT * FROM DateMe_User";
+		Statement statement = con.createStatement();
+		ResultSet result = statement.executeQuery(query);
+		List<User> userList = new ArrayList<User>();
+		if (result.next() == false) {
+
+			System.out.println("Result set is empty! User id does not exist");
+		} else {
+			do {
+				if (result.getString("password").equals(password)) {
+					User user = new User();
+					user.setFirstname(result.getString("FIRST_NAME"));
+					user.setUsername(result.getString("USERNAME"));
+					user.setSurname(result.getString("LAST_NAME"));
+					user.setEmail(result.getString("E_MAIL"));
+					user.setPassword(result.getString("PASSWORD"));
+					user.setDob(result.getString("DOB"));
+					user.setSex(result.getString("SEX"));
+					userList.add(user);
+				} else {
+					System.out.println("loda lehsun");
+				}
+			} while (result.next());
+		}
+
+		return userList;
+	}
+}
